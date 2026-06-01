0538d37437d02013734471b5253578f3412d3cd6
Oliver Hartley <o.hartley@me.com>
2026-06-01 17:16:10 +0100

    Minor demo_script updates

--- demo_script.R
+++ demo_script.R
@@ -14,7 +14,6 @@
 ## 3. Survey design: test_data/survey_design.csv
 source("ai_workflow_functions.R")
 start()
-
 # Step 2 - Simulate AI-Processing and output saving
 ## 1. Copy images to "Incoming Images" folder (code below)
 ## 2. Copy classifier outputs to "Incoming Images" folder
@@ -22,13 +21,13 @@ start()
 ai_output_files <- dir_ls("test_data", type = "file")[1:4]
 image_files <- dir_ls("test_data/NH7801/", type = "file")
 
-file_copy(ai_output_files, sub("test_data", "tester/Incoming Images", ai_output_files))
-file_copy(image_files, sub("test_data", "tester/Incoming Images", image_files))
+file_copy(ai_output_files, sub("test_data", "demo/Incoming Images", ai_output_files))
+file_copy(image_files, sub("test_data", "demo/Incoming Images", image_files))
 
-detections <- read.csv("tester/Incoming Images/results_files.csv", header = TRUE)
-detections$absolute_path <- paste0(getwd(), '/tester/Incoming Images')
-write.csv(detections, "tester/Incoming Images/results_files.csv", row.names = FALSE)
-classifications <- read.csv("tester/Incoming Images/results_detections.csv", header = TRUE)
-classifications$absolute_path <- paste0(getwd(), '/tester/Incoming Images')
-write.csv(classifications, "tester/Incoming Images/results_detections.csv", row.names = FALSE)
+detections <- read.csv("demo/Incoming Images/results_files.csv", header = TRUE)
+detections$absolute_path <- paste0(getwd(), '/demo/Incoming Images')
+write.csv(detections, "demo/Incoming Images/results_files.csv", row.names = FALSE)
+classifications <- read.csv("demo/Incoming Images/results_detections.csv", header = TRUE)
+classifications$absolute_path <- paste0(getwd(), '/demo/Incoming Images')
+write.csv(classifications, "demo/Incoming Images/results_detections.csv", row.names = FALSE)
 
